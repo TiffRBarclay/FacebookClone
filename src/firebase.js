ff6a187434fd14e5bd72bf28cbca943f6a31e0b5
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-07 18:17:42 +1200

    Add Firebase SDK

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAlrPHhzgXGuNHFa1-NoOeP8ydJ0drKoaY",
+  authDomain: "facebook-clone-f498b.firebaseapp.com",
+  databaseURL: "https://facebook-clone-f498b.firebaseio.com",
+  projectId: "facebook-clone-f498b",
+  storageBucket: "facebook-clone-f498b.appspot.com",
+  messagingSenderId: "630849970770",
+  appId: "1:630849970770:web:90811a694cf7050661f58b",
+  measurementId: "G-EG93HGR56W",
+};
