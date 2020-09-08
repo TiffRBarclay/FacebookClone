80f2442af6b0f17e279eecafefbc571295e8250e
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-08 14:11:09 +1200

    Create Post component

--- src/Feed.js
+++ src/Feed.js
@@ -3,12 +3,15 @@ import "./Feed.css";
 
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
+
+      <Post />
     </div>
   );
 }
