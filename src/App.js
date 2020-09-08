253e0bc3df05bb9786269021b3e4bd3bfcce9f6d
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-08 12:56:33 +1200

    Make sidebar and feed components visible on app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./Header";
+import SideBar from "./SideBar";
+import Feed from "./Feed";
+
 function App() {
   return (
     <div className="app">
-      <h1>Let's build the facebook clone</h1>
+      <Header />
+
+      <div className="app_body">
+        <SideBar />
+        <Feed />
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
