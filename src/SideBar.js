f240107e8c7beb942796c498a731650c1dcb7d4e
Tiffany Barclay <tiffany@barnet.co.nz>
2020-09-08 12:55:24 +1200

    Create side bar components

---
+++ src/SideBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Sidebar.css";
+
+import SidebarRow from "./SidebarRow";
+import FaceBookLogo from "./Images/hiclipart.com.png";
+
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreOutlined from "@material-ui/icons/ExpandMoreOutlined";
+
+function SideBar() {
+  return (
+    <div className="sidebar">
+      <SidebarRow src={FaceBookLogo} title="Tiffany Barclay" />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
+    </div>
+  );
+}
+
+export default SideBar;
